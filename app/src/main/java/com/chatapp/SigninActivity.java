6c4067ed828996761f860877e6bec176312ea1cc
Thanh Nguyen <thanhssvn@gmail.com>
2016-12-16 05:52:23 +0700

    + Login UI

--- app/src/main/java/com/chatapp/SigninActivity.java
+++ app/src/main/java/com/chatapp/SigninActivity.java
@@ -6,25 +6,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
+public class SigninActivity extends AppCompatActivity {
 
     private static final int RC_TO_REGISTER = 1;
 
+    @Bind(R.id.rg_login_type)
+    RadioGroup rgLoginType;
+    @Bind(R.id.v_input_email)
+    View vInputEmail;
+    @Bind(R.id.v_input_phone)
+    View vInputPhone;
+    @Bind(R.id.edt_phone)
+    EditText edtPhone;
     @Bind(R.id.edt_email)
     EditText edtEmail;
     @Bind(R.id.edt_password)
     EditText edtPassword;
-    @Bind(R.id.btn_login)
-    Button btnLogin;
-    @Bind(R.id.btn_to_register)
-    Button btnToCreateAccount;
-    @Bind(R.id.btn_forgot_password)
-    Button btnForgotPassword;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,22 +35,27 @@ public class SigninActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_signin);
         ButterKnife.bind(this);
 
-        btnLogin.setOnClickListener(this);
-        btnToCreateAccount.setOnClickListener(this);
-        btnForgotPassword.setOnClickListener(this);
+        rgLoginType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == R.id.rd_login_by_email) {
+                    vInputEmail.setVisibility(View.VISIBLE);
+                    vInputPhone.setVisibility(View.GONE);
+                } else {
+                    vInputEmail.setVisibility(View.GONE);
+                    vInputPhone.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_login:
-                startActivity(new Intent(SigninActivity.this, MyProfileActivity.class));
-                break;
-            case R.id.btn_to_register:
-                startActivityForResult(new Intent(SigninActivity.this, RegisterByEmailActivity.class), RC_TO_REGISTER);
-                break;
-            default:
-                break;
-        }
+    @OnClick(R.id.btn_login)
+    public void clickLogin(Button btnLogin) {
+        startActivity(new Intent(SigninActivity.this, MyProfileActivity.class));
+    }
+
+    @OnClick(R.id.btn_go_to_register)
+    public void clickGotoRegister(Button btnGotoRegister) {
+        startActivityForResult(new Intent(SigninActivity.this, RegisterByEmailActivity.class), RC_TO_REGISTER);
     }
 }
