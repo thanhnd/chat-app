4ef22e1ef5cbac9fbab00b4f4a994152cc649952
Thanh Nguyen <thanhssvn@gmail.com>
2016-12-24 00:38:17 +0700

    + Bottom bar

--- app/src/main/java/com/chatapp/mvp/home/HomeActivity.java
+++ app/src/main/java/com/chatapp/mvp/home/HomeActivity.java
@@ -1,11 +1,16 @@
 package com.chatapp.mvp.home;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 
 import com.chatapp.R;
 import com.chatapp.mvp.base.BaseActivity;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabSelectedListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -15,6 +20,10 @@ public class HomeActivity extends BaseActivity {
     @Bind(R.id.main_tool_bar)
     Toolbar toolbar;
 
+    @Bind(R.id.activity_main)
+    CoordinatorLayout coordinatorLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +31,7 @@ public class HomeActivity extends BaseActivity {
 
         ButterKnife.bind(this);
 
+        // Init toolbar
         setSupportActionBar(toolbar);
         if (toolbar != null) {
             toolbar.setContentInsetsAbsolute(0, 0);
@@ -30,5 +40,27 @@ public class HomeActivity extends BaseActivity {
         if (actionBar != null) {
             actionBar.setElevation(0);
         }
+
+        // Init bottom bar
+        BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
+        bottomBar.setItemsFromMenu(R.menu.menu_home, new OnMenuTabSelectedListener() {
+            @Override
+            public void onMenuItemSelected(@IdRes int menuItemId) {
+                switch (menuItemId) {
+                    case R.id.tab_item_nearyou:
+                        Snackbar.make(coordinatorLayout, "Recent Item Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                    case R.id.tab_item_chat:
+                        Snackbar.make(coordinatorLayout, "Favorite Item Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                    case R.id.tab_item_friends:
+                        Snackbar.make(coordinatorLayout, "Location Item Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                    case R.id.tab_item_favorites:
+                        Snackbar.make(coordinatorLayout, "Location Item Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        });
     }
 }
